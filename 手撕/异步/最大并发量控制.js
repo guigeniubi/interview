119c1e8c4c3a4df54255f7f01b9e1cc2a1382f44
zhouwenxuan <1042684036@qq.com>
2024-03-07 17:52:41 +0800

    update

---
+++ 手撕/异步/最大并发量控制.js
@@ -0,0 +1,29 @@
+async function fn(tasks, maxCount) {
+    // write code here
+
+
+    const results=[];
+    const executingtask=[];
+
+    for(task in tasks){
+        const taskPromise=task();
+        results.push(taskPromise);
+        if(executingtask>maxCount){
+            await Promise.race(executingtask);
+        }
+        executingtask.push(taskPromise);
+
+        //删除完成了的正在进行的Promise
+        await taskPromise.finally(()=>{
+            const index =executingtask.indexOf(taskPromise);
+            executingtask.splice(index,1)
+        })
+    }
+  }
+  
+  // test:
+  (async () => {
+    const results = await fn([fetch1, fetch2, fetch3, fetch4], 2);
+    console.log(results); // [1, 2, 3, 4]
+  })();
+  
