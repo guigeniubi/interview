7a68a16257e8af3271dba2d8d71915934fd72d9f
zhouwenxuan <1042684036@qq.com>
2024-03-13 20:06:37 +0800

    anchor

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Drag from './component/drag/drag.tsx'; // 确保路径和配置正确
 import Selectapp from './component/select/select.tsx';
 import ScrollFetch from './component/ScrollFetch/ScrollFetch.tsx';
+import AnchorPoint from './component/AnchorPoint/AnchorPoint.tsx';
 const App = () => {
   return (
     <Router>
@@ -11,6 +12,7 @@ const App = () => {
         {/* 在v6中，你使用element属性和JSX来指定组件 */}
         <Route path="/select" element={<Selectapp />} />
         <Route path="/Scroll" element={<ScrollFetch />} />
+        <Route path="/anchorPoint" element={ <AnchorPoint />} />
       </Routes>
     </Router>
   );
