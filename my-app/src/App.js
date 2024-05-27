659b7d537efa2a479cd63a741630daa875da2045
guigeniubi <1042684036@qq.com>
2024-05-28 02:03:34 +0800

    ys

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Drag from './component/drag/drag.tsx'; // 确保路径和配置正确
 import Selectapp from './component/select/select.tsx';
-import ScrollFetch from './component/ScrollFetch/ScrollFetch.tsx';
+import ListComponent from './component/ScrollFetch/fetch.jsx';
 import AnchorPoint from './component/AnchorPoint/AnchorPoint.tsx';
 import VirtualArray from './component/virtualArray/virtualArray.tsx';
 import UploadFile from './component/uploadFile/uploadFile.tsx';
@@ -13,7 +13,7 @@ const App = () => {
         <Route path="/drag" element={<Drag />} />
         {/* 在v6中，你使用element属性和JSX来指定组件 */}
         <Route path="/select" element={<Selectapp />} />
-        <Route path="/Scroll" element={<ScrollFetch />} />
+        <Route path="/ListComponent" element={<ListComponent />} />
         <Route path="/anchorPoint" element={ <AnchorPoint />} />
         <Route path="/virtualArray" element={ <VirtualArray />} />
         <Route path="/uploadFile" element={ <UploadFile />} />
